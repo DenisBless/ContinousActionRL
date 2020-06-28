dac9799c7a9102e4b20a89f59a8f9b3b338e3273
blessing <blessing.denis@gmx.de>
2020-06-28 11:36:13 +0200

    included mountain car env

--- experiments/main_mountain_car.py
+++ experiments/main_mountain_car.py
@@ -1,8 +1,10 @@
 from continous_action_RL.actor_critic_networks import Actor, Critic
-from continous_action_RL.off_policy.replay_buffer import *
+from continous_action_RL.off_policy.replay_buffer import ReplayBuffer
 from continous_action_RL.sampler import Sampler
+from continous_action_RL.evaluater import Evaluator
 from continous_action_RL.logger import Logger
 from continous_action_RL.off_policy.off_policy_learner import OffPolicyLearner
+import pathlib
 import gym
 
 if __name__ == '__main__':
@@ -14,27 +16,30 @@ if __name__ == '__main__':
     Constant Trajectory Length of 200
     Goal: Pendulum should stay upright
     """
-    # env = gym.make("Pendulum-v0")
     env = gym.make("MountainCarContinuous-v0")
 
     # PARAMETER
     NUM_OBSERVATIONS = env.observation_space.shape[0]
     NUM_ACTIONS = env.action_space.shape[0]
-    NUM_TRAJECTORIES = 300
-    BATCH_SIZE = 32
     TRAJECTORY_LENGTH = 1000
-    UPDATE_TARGNETS_EVERY = 50
-    NUM_TRAINING_ITERATIONS = 100
+    NUM_EVAL_TRAJECTORIES = 50
+    NUM_TRAJECTORIES = 32
+    BATCH_SIZE = 32
+    UPDATE_TARGNETS_EVERY = 1# 50
+    NUM_TRAINING_ITERATIONS = 1# 100
+    TOTAL_TIMESTEPS = 1000
     ACTOR_LEARNING_RATE = 2e-4
     CRITIC_LEARNING_RATE = 2e-4
+    ENTROPY_REGULARIZATION_ON = False
     ENTROPY_REGULARIZATION = 1e-5
     ACTION_STD_LOW = 1e-2
     ACTION_STD_HIGH = 1
-    ACTION_MEAN_SCALE = 1
-    ACTION_BOUNDS = (-1, 1)
+    ACTION_MEAN_SCALE = 2
+    ACTION_BOUNDS = (-2, 2)
     REPLAY_BUFFER_SIZE = 5000
     LOG_EVERY = 10
     SAVE_MODEL_EVERY = 10
+    MODEL_SAVE_PATH = str(pathlib.Path().absolute()) + "/models/"
 
     replay_buffer = ReplayBuffer(REPLAY_BUFFER_SIZE)
 
@@ -53,7 +58,7 @@ if __name__ == '__main__':
                       num_trajectories=NUM_TRAJECTORIES,
                       actor_network=actor,
                       replay_buffer=replay_buffer,
-                      render=True,
+                      render=False,
                       logger=logger)
 
     learner = OffPolicyLearner(actor=actor,
@@ -61,13 +66,25 @@ if __name__ == '__main__':
                                trajectory_length=TRAJECTORY_LENGTH,
                                actor_lr=ACTOR_LEARNING_RATE,
                                critic_lr=CRITIC_LEARNING_RATE,
+                               entropy_regularization_on=ENTROPY_REGULARIZATION_ON,
                                entropy_regularization=ENTROPY_REGULARIZATION,
                                update_targnets_every=UPDATE_TARGNETS_EVERY,
                                num_training_iter=NUM_TRAINING_ITERATIONS,
                                minibatch_size=BATCH_SIZE,
                                logger=logger)
 
-    for t in range(5000):
+    evaluator = Evaluator(env=env,
+                          actor=actor,
+                          critic=critic,
+                          save_path=MODEL_SAVE_PATH,
+                          num_trajectories=NUM_EVAL_TRAJECTORIES,
+                          save_model_every=SAVE_MODEL_EVERY,
+                          logger=logger,
+                          render=False)
+
+    for t in range(TOTAL_TIMESTEPS):
         print("-" * 10, t, "-" * 10)
         sampler.collect_trajectories()
         learner.learn(replay_buffer)
+        evaluator.evaluate()
+
