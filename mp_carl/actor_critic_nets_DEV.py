implemented correct logprob calc

mp_carl/actor_critic_nets_DEV.py
@@ -9,6 +9,14 @@ def init_weights(module: torch.nn.Module, gain: float = 1) -> None:
         module.bias.data.fill_(0.0)
 
 
+def normalize_actions(action: torch.Tensor, lower_bounds: List, upper_bounds: List) -> torch.Tensor:
+    ...
+
+
+def denormalize_actions(action: torch.Tensor, lower_bounds: List, upper_bounds: List) -> torch.Tensor:
+    ...
+
+
 class Actor(torch.nn.Module):
     def __init__(self,
                  num_actions: int,
@@ -20,6 +28,7 @@ class Actor(torch.nn.Module):
         super(Actor, self).__init__()
         self.log_std_init = log_std_init
         self.eps = eps
+        self.action_dim = num_actions
         if actor_layers is None:
             actor_layers = [64, 64]
 
@@ -33,22 +42,38 @@ class Actor(torch.nn.Module):
         self.model = torch.nn.Sequential(*actor_modules)
         init_weights(self.model)
 
-    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        mean = self.model(x)
-        log_std = torch.nn.Parameter(torch.ones(self.action_dim) * self.log_std_init, requires_grad=True)
-        return mean, log_std
+        self.log_std = torch.nn.Parameter(torch.ones(self.action_dim) * self.log_std_init, requires_grad=True)
 
-    def sample(self, mean: torch.Tensor, log_std: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        dist = self.normal_dist(mean, log_std).rsample()
+    def forward(self, obs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        mean = self.model(obs)
+        return mean, self.log_std
+
+    def action_sample(self, mean: torch.Tensor, log_std: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        dist = self.normal_dist(mean, log_std)
         normal_action = dist.rsample()  # rsample() employs reparameterization trick
         action = torch.tanh(normal_action)
-        normal_log_prob = dist.log_prob()
+        normal_log_prob = dist.log_prob(normal_action)
         log_prob = normal_log_prob - torch.sum(torch.log(1 - action.pow(2) + self.eps))
-        return action, log_prob
+        return 2*action, log_prob
+
+    def get_log_prob(self, actions: torch.Tensor, mean: torch.Tensor, log_std: torch.Tensor,
+                     normal_actions: torch.Tensor = None) -> torch.Tensor:
+        actions /= 2  # todo use normalize instead
+        if normal_actions is None:
+            normal_actions = self.inverseTanh(actions)
+
+        normal_log_probs = self.normal_dist(mean, log_std).log_prob(normal_actions)
+        log_probs = normal_log_probs - torch.sum(torch.log((1 - actions.pow(2) + self.eps)))
+        return log_probs
 
     @staticmethod
     def normal_dist(mean: torch.Tensor, log_std: torch.Tensor) -> Normal:
         return Normal(loc=mean, scale=torch.ones_like(mean) * log_std.exp())
+
+    @staticmethod
+    def inverseTanh(action: torch.Tensor, eps: float = 1e-6) -> torch.Tensor:
+        inv = 0.5 * (action.log1p() - (-action).log1p())
+        return inv.clamp(min=-1 + eps, max=1 - eps)
 
     def copy_params(self, source_network):
         for param, source_param in zip(self.parameters(), source_network.parameters()):
@@ -71,12 +96,13 @@ class Critic(torch.nn.Module):
         for i in range(len(critic_base) - 1):
             critic_modules.append(torch.nn.Linear(critic_base[i], critic_base[i + 1]))
             if i is not len(critic_base) - 2:
-                critic_modules.append(torch.nn.ReLU)
+                critic_modules.append(torch.nn.ReLU())
 
         self.model = torch.nn.Sequential(*critic_modules)
         init_weights(self.model)
 
-    def forward(self, x):
+    def forward(self, action, obs):
+        x = torch.cat([action/2, obs], dim=-1) # todo remove /2 by normalize_action()
         return self.model(x)
 
     def copy_params(self, source_network):
@@ -85,8 +111,9 @@ class Critic(torch.nn.Module):
 
 
 if __name__ == '__main__':
-    Critic(5, 5)
+    a = Actor(5, 5)
 
+    print()
     import torch.nn as nn
 
     # modules = []

mp_carl/agent.py
@@ -3,7 +3,8 @@ import copy
 from torch.utils.tensorboard import SummaryWriter
 
 from mp_carl.loss_fn import Retrace, ActorLoss
-from mp_carl.actor_critic_networks import Actor, Critic
+# from mp_carl.actor_critic_networks import Actor, Critic
+from mp_carl.actor_critic_nets_DEV import Actor, Critic
 import torch
 import gym
 import numpy as np
@@ -24,14 +25,17 @@ class Agent:
         self.env = gym.make("Pendulum-v0")
         self.num_actions = self.env.action_space.shape[0]
         self.num_obs = self.env.observation_space.shape[0]
-
         self.actor = Actor(num_actions=self.num_actions,
-                           num_obs=self.num_obs,
-                           mean_scale=arg_parser.action_mean_scale,
-                           std_low=arg_parser.action_std_low,
-                           std_high=arg_parser.action_std_high).to(self.device)
-
+                           num_obs=self.num_obs).to(self.device)
         self.critic = Critic(num_actions=self.num_actions, num_obs=self.num_obs).to(self.device)
+
+        # self.actor = Actor(num_actions=self.num_actions,
+        #                    num_obs=self.num_obs,
+        #                    mean_scale=arg_parser.action_mean_scale,
+        #                    std_low=arg_parser.action_std_low,
+        #                    std_high=arg_parser.action_std_high).to(self.device)
+        #
+        # self.critic = Critic(num_actions=self.num_actions, num_obs=self.num_obs).to(self.device)
         self.target_actor = copy.deepcopy(self.actor).to(self.device)
         self.target_critic = copy.deepcopy(self.critic).to(self.device)
 
@@ -67,7 +71,7 @@ class Agent:
             obs = torch.tensor(self.env.reset(), dtype=torch.float).to(self.device)
             done = False
             while not done:
-                mean, std = self.actor.forward(observation=obs)
+                mean, std = self.actor.forward(obs)
                 mean = mean.to(self.device)
                 std = std.to(self.device)
                 action, action_log_prob = self.actor.action_sample(mean, std)
@@ -168,8 +172,8 @@ class Agent:
             if self.logger is not None and i % self.log_every == 0:
                 self.logger.add_scalar(scalar_value=actor_loss.item(), tag="Loss/Actor_loss")
                 self.logger.add_scalar(scalar_value=critic_loss.item(), tag="Loss/Critic_loss")
+                self.logger.add_scalar(scalar_value=current_std, tag="Statistics/Action_std")
                 self.logger.add_histogram(values=current_mean, tag="Statistics/Action_mean")
-                self.logger.add_histogram(values=current_std, tag="Statistics/Action_std")
 
         self.actor.copy_params(self.param_server.shared_actor)
         self.critic.copy_params(self.param_server.shared_critic)
@@ -184,7 +188,7 @@ class Agent:
             rewards = []
             done = False
             while not done:
-                mean, std = self.actor.forward(observation=obs)
+                mean, std = self.actor.forward(obs)
                 mean = mean.to(self.device)
                 std = std.to(self.device)
                 action, action_log_prob = self.actor.action_sample(mean, std)
@@ -215,4 +219,3 @@ class Agent:
     def reset_grad(net):
         for param in net.parameters():
             param.grad = torch.zeros_like(param.data, dtype=torch.float32)
-

mp_carl/parameter_server.py
@@ -1,4 +1,5 @@
-from mp_carl.actor_critic_networks import Actor, Critic
+# from mp_carl.actor_critic_networks import Actor, Critic
+from mp_carl.actor_critic_nets_DEV import Actor, Critic
 from mp_carl.optimizer import SharedAdam
 import torch
 
@@ -12,11 +13,14 @@ class ParameterServer:
         self.N = torch.tensor(0)  # current number of gradients
         self.N.share_memory_()
         self.lock = lock  # enter monitor to prevent race condition
+        # self.shared_actor = Actor(num_actions=num_actions,
+        #                           num_obs=num_obs,
+        #                           mean_scale=arg_parser.action_mean_scale,
+        #                           std_low=arg_parser.action_std_low,
+        #                           std_high=arg_parser.action_std_high).to(device)
+
         self.shared_actor = Actor(num_actions=num_actions,
-                                  num_obs=num_obs,
-                                  mean_scale=arg_parser.action_mean_scale,
-                                  std_low=arg_parser.action_std_low,
-                                  std_high=arg_parser.action_std_high).to(device)
+                                  num_obs=num_obs).to(device)
         self.shared_actor.share_memory()
         self.shared_critic = Critic(num_actions=num_actions, num_obs=num_obs).to(device)
         self.shared_critic.share_memory()

todos.py
@@ -7,16 +7,16 @@
 #  position of the robot
 # • Todo: Try using a "burn in" phase where we train the critic a bit on policy to prevent the retrace weights from
 #  going to zero
-# • Todo: Implement torch.rsample. Enables reparameterization trick
+# • Todo: Implement torch.rsample. Enables reparameterization trick -> DONE
 # • Todo: Try out prioritized experience replay
 # • Todo: Try out trust region updates
 # • Todo: Try out learning rate decay
-# • Todo: Try out orthogonal weight init and bias zero
+# • Todo: Try out orthogonal weight init and bias zero -> DONE
 # • Todo: Try to out entropy regularization
 # • Todo: Try out increasing dt such that sampling an episode requires less time
 # • Todo: Try out truncating the gaussian distribution directly at the output of the neural network
 # • Todo: Make standard deviation state independent
-# • Todo: Try out reward clippint (at least for pendulum)
+# • Todo: Try out reward clipping (at least for pendulum)
 # • Todo:
 
 