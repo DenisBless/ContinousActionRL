ae45facc5ec2f4929da82d2c17ccbb563b6eb34c
blessing <blessing.denis@gmx.de>
2020-07-15 11:45:43 +0200

    added comments

--- model_scripts/model1.sh
+++ model_scripts/model1.sh
@@ -1,11 +1,11 @@
 python3 ../mp_carl/main.py \
---num_worker=6 \
---num_grads=12 \
+--num_worker=1 \
+--num_grads=1 \
 --update_targnets_every=200     \
 --learning_steps=2000 \
 --actor_lr=2e-4 \
 --critic_lr=2e-4 \
 --global_gradient_norm=1 \
 --entropy_reg=0 \
---replay_buffer_size=1000 \
+--replay_buffer_size=300 \
 --num_trajectories=20
